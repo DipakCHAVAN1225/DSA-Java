f5fc468fc9c7ab9f571ce6bb5b371f19251eec00
Dipak Chavan <109498762+DipakCHAVAN1225@users.noreply.github.com>
2026-09-09 19:22:08 +0530

    [69] [Time Beats: 98.99%] [Memory Beats: 78.72%] - LeetPush

---
+++ Arrays/69-Sqrt(x).java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int ans = 0;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x / mid) {
+                // mid * mid <= x
+                ans = mid;
+                left = mid + 1;
+            } else {
+                // mid * mid > x
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
